4217ce701caf86323811be388564a4bfb60dbc0b
Thịnh Trần <thinhtran3803@gmail.com>
2023-09-02 04:07:01 +0700

    add config cors

--- Back-end/src/main/java/com/example/foodordering/BackEndApplication.java
+++ Back-end/src/main/java/com/example/foodordering/BackEndApplication.java
@@ -2,6 +2,9 @@ package com.example.foodordering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackEndApplication {
@@ -10,4 +13,16 @@ public class BackEndApplication {
         SpringApplication.run(BackEndApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
