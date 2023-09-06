a96d4d9e306ade751aa9bc7e644207862db9c581
Thịnh Trần <thinhtran3803@gmail.com>
2023-09-06 09:03:55 +0700

    refactor

--- Back-end/src/main/java/com/example/foodordering/services/BookingService.java
+++ Back-end/src/main/java/com/example/foodordering/services/BookingService.java
@@ -10,10 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+
+import static com.example.foodordering.constant.ReservationStatus.*;
 
 @Service
 public class BookingService {
@@ -22,13 +25,21 @@ public class BookingService {
 
     @Autowired
     private ReservationRepository reservationRepository;
+    
+    private Customer saveOrUpdateCustomer(Customer customer) {
+        if (customer != null) {
+            Optional<Customer> existingCustomer = customerRepository.findByPhone(customer.getPhone());
+            return existingCustomer.orElseGet(() -> customerRepository.save(customer));
+        }
+        return null;
+    }
 
     public ResponseEntity<ResponseObject> createReservation(Reservation reservation) {
         try {
             Customer customer = saveOrUpdateCustomer(reservation.getCustomer());
 
             reservation.setCustomer(customer);
-            reservation.setStatus(ReservationStatus.Pending);
+            reservation.setStatus(Pending);
             reservationRepository.save(reservation);
 
             return ResponseEntity.status(HttpStatus.OK).body(
@@ -41,19 +52,13 @@ public class BookingService {
         }
     }
 
-    private Customer saveOrUpdateCustomer(Customer customer) {
-        if (customer != null) {
-            Optional<Customer> existingCustomer = customerRepository.findByPhone(customer.getPhone());
-            if (existingCustomer.isPresent()) {
-                return existingCustomer.get();
-            } else {
-                return customerRepository.save(customer);
-            }
-        }
-        return null;
-    }
 
-    public List<Object[]> getCustomerAndReservationTime(){
-        return reservationRepository.getCustomerAndReservationTime();
+
+    public List<Object[]> getCustomerAndReservationTime(@RequestParam(value = "filterByStatus", required = false) ReservationStatus filterByStatus){ // filter By Status
+        if (filterByStatus == null) {
+            return reservationRepository.getCustomerAndReservationTime();
+        } else {
+            return reservationRepository.getCustomerNameAndPhoneByStatus(filterByStatus);
+        }
     }
 }
