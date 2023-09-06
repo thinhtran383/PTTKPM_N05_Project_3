8892fc4759fde4f604c776f14c7e0a1b9e14242a
Thịnh Trần <thinhtran3803@gmail.com>
2023-09-06 15:21:42 +0700

    get all employee

--- Back-end/src/main/java/com/example/foodordering/controller/EmployeeController.java
+++ Back-end/src/main/java/com/example/foodordering/controller/EmployeeController.java
@@ -6,10 +6,7 @@ import com.example.foodordering.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -54,6 +51,13 @@ public class EmployeeController {
                     new ResponseObject("failed", "Login failed", "")
             );
         }
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ResponseObject> getAllEmployees(){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Query successfully", employeeService.getAllEmployees())
+        );
     }
 
 
--- Back-end/src/main/java/com/example/foodordering/repositories/EmployeeRepository.java
+++ Back-end/src/main/java/com/example/foodordering/repositories/EmployeeRepository.java
@@ -5,9 +5,13 @@ import com.example.foodordering.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee findByUsername(String username);
 
     @Query("select e.role from Employee e where e.username =:username ")
     EmployeeRole findRoleByUsername(String username);
+
+    List<Employee> findByRole(EmployeeRole role);
 }
