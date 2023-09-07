7e4e84468d06b750dea65d38907b347fcc3ad1bf
Thịnh Trần <thinhtran3803@gmail.com>
2023-09-07 21:23:35 +0700

    find image

--- Back-end/src/main/java/com/example/foodordering/repositories/MenuItemsRepository.java
+++ Back-end/src/main/java/com/example/foodordering/repositories/MenuItemsRepository.java
@@ -2,10 +2,13 @@ package com.example.foodordering.repositories;
 
 import com.example.foodordering.entity.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface MenuItemsRepository extends JpaRepository<MenuItem, Long> {
-    List<MenuItem> findByName(String name);
-    List<MenuItem>findMenuItemByName(String name);
+    List<MenuItem> findMenuItemByName(String name);
+
+    @Query("Select m.imageUrl from MenuItem m where m.id = :id")
+    String findAllImageUrl(Long id);
 }
