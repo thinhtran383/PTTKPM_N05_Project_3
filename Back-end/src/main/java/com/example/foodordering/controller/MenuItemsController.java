bb6a97a39087beaa41f754aa5b740390fe571fe5
Thịnh Trần <thinhtran3803@gmail.com>
2023-09-07 21:22:38 +0700

    update get menu and image

--- Back-end/src/main/java/com/example/foodordering/controller/MenuItemsController.java
+++ Back-end/src/main/java/com/example/foodordering/controller/MenuItemsController.java
@@ -1,32 +1,49 @@
 package com.example.foodordering.controller;
 
 import com.example.foodordering.entity.MenuItem;
-import com.example.foodordering.entity.ResponseObject;
-import com.example.foodordering.repositories.MenuItemsRepository;
+import com.example.foodordering.models.ResponseObject;
 import com.example.foodordering.services.MenuItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.MalformedURLException;
 
 @RestController
 @RequestMapping("api/menu")
 public class MenuItemsController {
-    @Autowired
-    private MenuItemsRepository menuItemsRepository;
 
     @Autowired
-    private MenuItemsService menuItemsService;
+    private MenuItemsService menuItemService;
 
-    @GetMapping("")
-    public ResponseEntity<ResponseObject> getAllMenuItems(){
-        return ResponseEntity.status(HttpStatus.OK).body(
-                new ResponseObject("ok", "Query successfully", menuItemsRepository.findAll())
-        );
-    }
 
     @PostMapping("/add")
     public ResponseEntity<ResponseObject> addNewItem(@RequestBody MenuItem menuItem){
-        return menuItemsService.addNewItem(menuItem);
+        return menuItemService.addNewItem(menuItem);
     }
+
+    @GetMapping("")
+    public ResponseEntity<ResponseObject> getAllMenu(){
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok","Query successfully", menuItemService.getAllMenu())
+        );
+    }
+
+    @GetMapping("/{id}.jpg")
+    public ResponseEntity<?> getImageById(@PathVariable Long id) throws MalformedURLException {
+        Resource image = menuItemService.getImageById(id);
+        if(image != null){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(image);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                  new ResponseObject("false", "Image not found", "")
+        );
+    }
+
 }
--- Back-end/src/main/java/com/example/foodordering/services/MenuItemsService.java
+++ Back-end/src/main/java/com/example/foodordering/services/MenuItemsService.java
@@ -1,34 +1,63 @@
 package com.example.foodordering.services;
 
 import com.example.foodordering.entity.MenuItem;
-import com.example.foodordering.entity.ResponseObject;
+import com.example.foodordering.models.MenuItemDTO;
+import com.example.foodordering.models.ResponseObject;
 import com.example.foodordering.repositories.MenuItemsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.List;
+import java.util.ArrayList;
 
 @Service
 public class MenuItemsService {
+
+    @Value("${image.upload.path}")
+    private String imageUploadPath;
+
     @Autowired
-    private MenuItemsRepository menuItemsRepository;
+    private MenuItemsRepository menuItemRepository;
 
-    public ResponseEntity<ResponseObject> addNewItem(@RequestBody MenuItem menuItem){
-        List<MenuItem> isExist = menuItemsRepository.findMenuItemByName(menuItem.getName().trim());
+    public ResponseEntity<ResponseObject> addNewItem(@RequestBody MenuItem menuItem) {
+        List<MenuItem> isExist = menuItemRepository.findMenuItemByName(menuItem.getName().trim());
 
-        if(isExist.size() > 0){
+        if (isExist.size() > 0) {
             return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(
-              new ResponseObject("false","Item is existed", "")
-            );
-        }
-        else {
-
+                    new ResponseObject("false", "Item is existed", ""));
+        } else {
             return ResponseEntity.status(HttpStatus.OK).body(
-                    new ResponseObject("ok", "Query successfully", menuItemsRepository.save(menuItem))
-            );
+                    new ResponseObject("ok", "Query successfully", menuItemRepository.save(menuItem)));
         }
     }
+
+    public List<MenuItem> getAllMenu() {
+        return menuItemRepository.findAll();
+    }
+
+    public Resource getImageById(Long id) throws MalformedURLException {
+        Path imagePath = Paths.get(imageUploadPath);
+
+        Resource resource = new UrlResource(imagePath.toUri() + "/" + menuItemRepository.findAllImageUrl(id));
+
+        if(resource.exists() && resource.isReadable()){
+            return resource;
+        }
+
+        return null;
+    }
+
 }
