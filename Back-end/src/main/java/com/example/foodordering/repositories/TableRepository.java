0a2f0edfde0d57a2de13c911274387ab55c41c57
Thịnh Trần <thinhtran3803@gmail.com>
2023-09-13 14:12:20 +0700

    add: query findTableById

--- Back-end/src/main/java/com/example/foodordering/repositories/TableRepository.java
+++ Back-end/src/main/java/com/example/foodordering/repositories/TableRepository.java
@@ -5,7 +5,10 @@ import com.example.foodordering.entity.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TableRepository extends JpaRepository<Table, Long> {
     List<Table> findAllTableByStatus(TableStatus status);
+    Table findTableById(Long tableId);
+
 }
