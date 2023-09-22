9f435c81e605e29b7b100f7d7877358c80891e7e
Thịnh Trần <thinhtran3803@gmail.com>
2023-09-22 20:52:07 +0700

    test ws

--- Back-end/src/main/java/com/example/foodordering/configuration/WSConfig.java
+++ Back-end/src/main/java/com/example/foodordering/configuration/WSConfig.java
@@ -3,16 +3,19 @@ package com.example.foodordering.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WSConfig implements WebSocketMessageBrokerConfigurer {
 
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/ws");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
