707f6996050c86db9cfc26bed7897d860dd2ab74
Thịnh Trần <thinhtran3803@gmail.com>
2023-09-22 20:52:16 +0700

    no message

--- Back-end/src/main/java/com/example/foodordering/BackEndApplication.java
+++ Back-end/src/main/java/com/example/foodordering/BackEndApplication.java
@@ -20,6 +20,7 @@ public class BackEndApplication {
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
                         .allowedOrigins("*")
+                        .allowedOrigins("http://127.0.0.1:5500")
                         .allowedHeaders("*")
                         .allowedMethods("*");
             }
