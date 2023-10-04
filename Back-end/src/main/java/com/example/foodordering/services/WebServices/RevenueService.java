57aad84d2f510a318e1b2205663bc83c64c5459d
Hoanghaine <dohoanghai08062003@gmail.com>
2023-10-04 10:46:36 +0700

    add get this-week-amount

--- Back-end/src/main/java/com/example/foodordering/services/WebServices/RevenueService.java
+++ Back-end/src/main/java/com/example/foodordering/services/WebServices/RevenueService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.foodordering.DTO.RevenueDTO;
+import com.example.foodordering.DTO.WeeklyRevenueDTO;
 import com.example.foodordering.repositories.RevenueRepository;
 
 @Service
@@ -25,17 +26,19 @@ public class RevenueService {
         return convertToRevenueDTO(monthlyRevenueData);
     }
 
-    public List<RevenueDTO> getWeeklyRevenue(int month, int year) {
-        List<Object[]> weeklyRevenueData = revenueRepository.getRevenueAmountsByWeekForMonthAndYear(month, year);
-        return convertToRevenueDTO(weeklyRevenueData);
+    public List<RevenueDTO> getThisWeekAmount() {
+        List<Object[]> dailyRevenuesData = revenueRepository.getDailyRevenuesForCurrentWeek();
+        return convertToRevenueDTO(dailyRevenuesData);
     }
 
+    // Helper method to convert revenue data to RevenueDTO with consecutive IDs
     private List<RevenueDTO> convertToRevenueDTO(List<Object[]> revenueData) {
         List<RevenueDTO> revenueDTOList = new ArrayList<>();
+        int idCounter = 1; // Initialize the ID counter
 
         for (Object[] data : revenueData) {
             RevenueDTO revenueDTO = new RevenueDTO();
-            revenueDTO.setId((Integer) data[0]);
+            revenueDTO.setId(idCounter++); // Assign the next ID in sequence
             revenueDTO.setAmount((BigDecimal) data[1]);
 
             revenueDTOList.add(revenueDTO);
@@ -43,4 +46,5 @@ public class RevenueService {
 
         return revenueDTOList;
     }
+
 }
